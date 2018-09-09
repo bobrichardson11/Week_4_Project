e8ecf4e0236030feacc859200e938327032770ca
Unknown <bobrichardson65@gmail.com>
2018-09-09 19:13:15 -0400

    Initial commit

---
+++ run_analysis.r
@@ -0,0 +1,21 @@
+setwd("~/R/Week4_project/UCI HAR Dataset/combine")
+
+## Merge training and test datasets
+
+mydatatest <- read.table("x_test.txt", header = FALSE)
+mydatatrain <- read.table("x_train.txt", header = FALSE)
+alldata <- rbind(mydatatest, mydatatrain)
+##write.csv(alldata, "all.csv")
+
+## Extract mean and standard deviation measures for each measurement
+
+mean_values <- lapply(alldata, mean)
+sd_values <- lapply(alldata, sd)
+
+## Use descriptive activity names to name the activities in the data set
+## use  names(alldata) [x] <- "text"
+
+## Appropriately labels the data set with descriptive variable names.
+
+
+##From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
